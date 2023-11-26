f4d5e0782381efbc77c03df938383ad30006b12f
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-26 23:33:30 +0600

    firebase,router,tailwind-daisyui

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbLmbeudIZtR9BU_eOBgkiTHx23hzHUNk",
+  authDomain: "event-management-71.firebaseapp.com",
+  projectId: "event-management-71",
+  storageBucket: "event-management-71.appspot.com",
+  messagingSenderId: "648060716961",
+  appId: "1:648060716961:web:5c5abc88cbfb44d54d6de0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
