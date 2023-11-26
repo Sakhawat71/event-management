c358b3664fb405f2a15c85acfcbb20f0a4d76b86
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-26 23:58:08 +0600

    hide firebase config data

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbLmbeudIZtR9BU_eOBgkiTHx23hzHUNk",
-  authDomain: "event-management-71.firebaseapp.com",
-  projectId: "event-management-71",
-  storageBucket: "event-management-71.appspot.com",
-  messagingSenderId: "648060716961",
-  appId: "1:648060716961:web:5c5abc88cbfb44d54d6de0"
+  apiKey: import.meta.env.VITE_APIKEY, 
+  authDomain: import.meta.env.VITE_AUTHDOMAIN, 
+  projectId: import.meta.env.VITE_PROJECTID, 
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET, 
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID, 
+  appId: import.meta.env.VITE_APPID, 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
