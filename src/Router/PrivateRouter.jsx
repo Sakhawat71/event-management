270a132456e51e7244d811b6564171eb8a3b4670
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-27 10:44:23 +0600

    Private Router setup

--- src/Router/PrivateRouter.jsx
+++ src/Router/PrivateRouter.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { AuthContext } from '../AuthProvider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRouter = ({children}) => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+    const location = useLocation()
+    const {user,loading} = useContext(AuthContext);
+
+    if(loading){
+        return <span className="loading loading-bars loading-lg"></span>
+    }
+
+    if(user){
+        return children;
+    }
+
+    return <Navigate state={location.pathname}></Navigate>
 };
 
 PrivateRouter.propTypes = {
