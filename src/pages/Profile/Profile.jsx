2ed8e879d51e409a431e52937a5fca48eacba7d5
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-29 04:58:23 +0600

    update profile and display user data on profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,13 +3,15 @@ import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Profile = () => {
 
-    const {user} = useContext(AuthContext)
-    console.log(user)
+    const { user } = useContext(AuthContext)
+    // console.log(user)
     return (
         <div className="h-screen flex flex-col justify-center items-center mx-auto">
-            <figure>
-                <img src={user?.photoURL} alt="" />
-            </figure>
+            <div className="avatar">
+                <div className="w-24 rounded-xl">
+                    <img src={user?.photoURL} />
+                </div>
+            </div>
             <h4 className="text-xl font-semibold">{user?.displayName}</h4>
             <p className="text-blue-600 text-xl">{user?.email}</p>
             <p className="font-semi-bold">{user?.uid}</p>
